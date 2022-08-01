eba552f17325f41e384ab4ede0c30f92f30b555e
sergeevsn <84204812+sergeevsn@users.noreply.github.com>
2022-08-01 14:22:34 +0200

    Update settings.py

--- regressor_ui/settings.py
+++ regressor_ui/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y299x2l&(a0i6l!jbl-=21rrem$0$x%42$a1!&)e=(wdft-v2-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
